3755685d1f05b1f0105f3a887ce6ad1adcb5f47e
Anna <devilflow3@gmail.com>
2024-04-29 16:06:24 +1000

    Update games.js

--- middlewares/games.js
+++ middlewares/games.js
@@ -17,6 +17,7 @@ const getAllGames = async (req, res, next) => {
     req.isNew = !Boolean(req.games.find(item => item.title === req.body.title));
     next();
   };
+    
   
   const updateGamesArray = (req, res, next) => {
     if (req.isNew) {
